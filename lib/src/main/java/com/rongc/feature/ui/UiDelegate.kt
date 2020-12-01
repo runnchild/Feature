4bce8af3162a329bebc35e67f51025a6518529a9
rongc <qiurongc@foxmail.com>
2020-12-01 19:45:41 +0800

    修复多个地方订阅同一事件，当一个地方销毁时此事件全被移除问题

--- lib/src/main/java/com/rongc/feature/ui/UiDelegate.kt
+++ lib/src/main/java/com/rongc/feature/ui/UiDelegate.kt
@@ -59,6 +59,7 @@ open class UiDelegate<M : BaseViewModel<out BaseModel>>(val api: IUI<M>, action:
 
     open fun initObserver(owner: LifecycleOwner, viewModel: M) {
         viewModel.mainScope.onCreate()
+        viewModel.onCreate()
         api.getLifecycle().addObserver(viewModel)
         viewModel.dialogVisible.observe(owner, Observer {
             if (it) {
