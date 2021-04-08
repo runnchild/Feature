3aed90d2363988740fd6f5dc31ff4dcb2b6019a8
rongc <qiurongc@foxmail.com>
2021-04-08 11:38:04 +0800

    增加DrawableCenterTextView

---
+++ lib/src/main/java/com/rongc/feature/widget/DrawableCenterTextView.java
@@ -0,0 +1,52 @@
+package com.rongc.feature.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.Gravity;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class DrawableCenterTextView extends AppCompatTextView {
+    public DrawableCenterTextView(Context context) {
+        this(context, null);
+    }
+
+    public DrawableCenterTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DrawableCenterTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Drawable[] drawables = getCompoundDrawables();
+        if (null != drawables) {
+            Drawable drawableLeft = drawables[0];
+            Drawable drawableRight = drawables[2];
+            float textWidth = getPaint().measureText(getText().toString());
+            if (null != drawableLeft) {
+                setGravity(Gravity.START | Gravity.CENTER_VERTICAL);
+                float contentWidth = textWidth + getCompoundDrawablePadding() + drawableLeft.getIntrinsicWidth();
+                if (getWidth() - contentWidth > 0) {
+                    canvas.translate((getWidth() - contentWidth - getPaddingRight() - getPaddingLeft()) / 2, 0);
+                }
+            }
+            if (null != drawableRight) {
+                setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
+                float contentWidth = textWidth + getCompoundDrawablePadding() + drawableRight.getIntrinsicWidth();
+                if (getWidth() - contentWidth > 0) {
+                    canvas.translate(-(getWidth() - contentWidth - getPaddingRight() - getPaddingLeft()) / 2, 0);
+                }
+            }
+            if (null == drawableRight && null == drawableLeft) {
+                setGravity(Gravity.CENTER);
+            }
+        }
+        super.onDraw(canvas);
+
+    }
+}
