5c49423af6eeba244fe0cffb0ea74957b17a00b0
rongc <qiurongc@foxmail.com>
2021-07-22 20:08:02 +0800

    SingleLiveData移动至feature模块

--- feature/src/main/java/com/rongc/feature/SingleLiveData.kt
+++ feature/src/main/java/com/rongc/feature/SingleLiveData.kt
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.rongc.feature.bus
+package com.rongc.feature
 
 import android.util.Log
 import androidx.annotation.MainThread
