e97675fe1d0ae49754cdcdcaad58c3da29d18080
rongc <qiurongc@foxmail.com>
2021-07-26 19:28:00 +0800

    去除未设置text就自动隐藏控件的设定

--- emptyview/src/main/java/com/runnchild/emptyview/EmptyView.kt
+++ emptyview/src/main/java/com/runnchild/emptyview/EmptyView.kt
@@ -3,19 +3,16 @@ package com.runnchild.emptyview
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.runnchild.emptyview.databinding.EmptyViewBinding
 
-@BindingAdapter("textBuilder", "defaultClick", requireAll = false)
-fun TextView.textBuilder(builder: (TextView.() -> Unit)? = null, defaultClick: (() -> Unit)?) {
+@BindingAdapter("btnVisible", "textBuilder", "defaultClick", requireAll = false)
+internal fun TextView.textBuilder(visible: Boolean?, builder: (TextView.() -> Unit)? = null, defaultClick: (() -> Unit)?) {
     builder?.let {
         apply(builder)
-        isVisible = !text.isNullOrEmpty()
         if (!hasOnClickListeners() && defaultClick != null) {
             setOnClickListener {
                 defaultClick.invoke()
@@ -29,11 +26,6 @@ fun ImageView.imageBuilder(builder: (ImageView.() -> Unit)? = null) {
     builder?.let {
         apply(builder)
     }
-}
-
-@BindingAdapter("visible")
-internal fun View.visible(visible: Boolean) {
-    isVisible = visible
 }
 
 class EmptyView @JvmOverloads constructor(
