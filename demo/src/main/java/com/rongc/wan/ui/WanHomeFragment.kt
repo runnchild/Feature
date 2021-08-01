13d2ff3a6d78f12fef371030cdb970d6227f7b92
qiurc <qiurongc@foxmail.com>
2021-08-01 23:13:59 +0800

    whenSuccess更名doOnSuccess

--- demo/src/main/java/com/rongc/wan/ui/WanHomeFragment.kt
+++ demo/src/main/java/com/rongc/wan/ui/WanHomeFragment.kt
@@ -7,8 +7,8 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.rongc.demo.databinding.FragmentWanHomeBinding
-import com.rongc.feature.repository.whenSuccess
 import com.rongc.feature.ui.BaseFragment
+import com.rongc.feature.vo.doOnSuccess
 import com.rongc.list.ability.IPagerHost
 import com.rongc.list.ability.PagerAbility
 import com.rongc.list.viewpager2.BaseFragmentPagerAdapter
@@ -28,7 +28,7 @@ class WanHomeFragment : BaseFragment<FragmentWanHomeBinding, WanHomeViewModel>()
 
         // 分类数据请求结果订阅，ignoreLoading()为忽略加载中状态只关心结果
         // whenSuccess，只在请求成功后接收通知
-        viewModel.result.whenSuccess(lifecycleOwner) {
+        viewModel.result.doOnSuccess {
             // 配置TabLayout
             it.data?.forEach { project ->
                 val tab = mBinding.tabStrip.newTab()
